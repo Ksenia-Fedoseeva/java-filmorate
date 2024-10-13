3a4c848e19cf894f9f16779efda9b5761f3c89fd
Ksenia Fedoseeva <merzlikina.ksu@gmail.com>
2024-10-13 20:20:51 +0300

    Реализация ТЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     Collection<Film> getAllFilms();
+
     Film getFilmById(Long id);
+
     void deleteFilm(Long id);
+
     void checkFilmExist(Long id);
 }
